639d42e8e1cc9a248914a12c0a6db0f764bff2c2
Nikola Dulev <nikoladulev13@gmail.com>
2024-09-08 17:08:49 +0200

    Commit Exercises

---
+++ DSA src/Ispit/Zadaca19/FindMergeNode.java
@@ -0,0 +1,224 @@
+package Ispit.Zadaca19;
+
+import java.util.Scanner;
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class SLL<E> {
+    private SLLNode<E> first;
+
+    public SLL() {
+        this.first = null;
+    }
+
+    public void deleteList() {
+        first = null;
+    }
+
+    public int length() {
+        int ret;
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret = 1;
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret++;
+            }
+            return ret;
+        } else
+            return 0;
+
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret += tmp;
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "->" + tmp;
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public void insertFirst(E o) {
+        SLLNode<E> ins = new SLLNode<E>(o, first);
+        first = ins;
+    }
+
+    public void insertAfter(E o, SLLNode<E> node) {
+        if (node != null) {
+            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
+            node.succ = ins;
+        } else {
+            System.out.println("Dadenot jazol e null");
+        }
+    }
+
+    public void insertBefore(E o, SLLNode<E> before) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if (first == before) {
+                this.insertFirst(o);
+                return;
+            }
+            while (tmp.succ != before)
+                tmp = tmp.succ;
+            if (tmp.succ == before) {
+                SLLNode<E> ins = new SLLNode<E>(o, before);
+                tmp.succ = ins;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+    }
+
+    public void insertLast(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            SLLNode<E> ins = new SLLNode<E>(o, null);
+            tmp.succ = ins;
+        } else {
+            insertFirst(o);
+        }
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            first = first.succ;
+            return tmp.element;
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+    }
+
+    public E delete(SLLNode<E> node) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if (first == node) {
+                return this.deleteFirst();
+            }
+            while (tmp.succ != node && tmp.succ.succ != null)
+                tmp = tmp.succ;
+            if (tmp.succ == node) {
+                tmp.succ = tmp.succ.succ;
+                return node.element;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+                return null;
+            }
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+    }
+
+    public SLLNode<E> getFirst() {
+        return first;
+    }
+
+    public SLLNode<E> find(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.element != o && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element == o) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return first;
+    }
+
+    public void mirror() {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            SLLNode<E> newsucc = null;
+            SLLNode<E> next;
+
+            while (tmp != null) {
+                next = tmp.succ;
+                tmp.succ = newsucc;
+                newsucc = tmp;
+                tmp = next;
+            }
+            first = newsucc;
+        }
+    }
+}
+
+public class FindMergeNode {
+
+    public static void intersection(SLL<Integer> list1, SLL<Integer> list2, int intersectVal) {
+        SLLNode<Integer> current1 = list1.getFirst();
+        SLLNode<Integer> current2 = list2.getFirst();
+        int length1 = list1.length();
+        int length2 = list2.length();
+
+        if (length1 > length2) {
+            for (int i = 0; i < length1 - length2; i++) {
+                current1 = current1.succ;
+            }
+        } else {
+            for (int i = 0; i < length2 - length1; i++) {
+                current2 = current2.succ;
+            }
+        }
+
+        while (current1 != null && current2 != null) {
+            if (current1 == current2) {
+                intersectVal = current1.element;
+            }
+            current1 = current1.succ;
+            current2 = current2.succ;
+        }
+
+        System.out.println("Intersected at: " + intersectVal);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        SLL<Integer> list1 = new SLL<Integer>();
+        SLL<Integer> list2 = new SLL<Integer>();
+        int intersectVal = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Enter elements for the list (separated by spaces): ");
+        String[] elements1 = scan.nextLine().split(" ");
+        String[] elements2 = scan.nextLine().split(" ");
+
+        for (String element : elements1) {
+            list1.insertLast(Integer.parseInt(element));
+        }
+        for (String element : elements2) {
+            list2.insertLast(Integer.parseInt(element));
+        }
+
+        intersection(list1, list2, intersectVal);
+    }
+}
